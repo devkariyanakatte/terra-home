180feda6241def41dfee0564d949112fcd2fc3d3
Kariyanakatte <KariyanakatteM@bsg.ad.adp.com>
2018-08-12 18:17:02 -0400

    terra

---
+++ sandbox.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+        access_key                      = "${var.access_key}"
+        secret_key                      = "${var.secret_key}"
+        region                          = "${var.region}"
+}
+
+resource "aws_instance" "sandbox" {
+        ami                             = "ami-6871a115"
+        availability_zone               = "us-east-2b"
+        root_block_device {
+                volume_type             = "gp2"
+                volume_size             = "80"
+                
+        }
+        instance_type                   = "m4.large"
+        key_name                        = "jenkins-git"
+        count                           = 1
+
+  tags {
+    	Environment = "sandbox"
+	OS = "RHEL"
+	Name = "sandbox2"
+  }
+
+}
+
+
